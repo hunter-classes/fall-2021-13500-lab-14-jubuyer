e25392a8f5447f8a0eb3b993fe04c66d0d2e5b96
jubuyer <jahmed10@stuy.edu>
2022-01-03 13:51:01 -0500

    prototyping and inclusions

--- funcs.h
+++ funcs.h
@@ -5,7 +5,14 @@
 template <class T>
 class MyVector {
 public:
-
+  int size();
+  int capacity();
+  bool empty();
+  void push_back(T item);
+  void pop_back(int n);
+  void pop_back();
+  void clear();
+  T operator[](int n);
 private:
 
 }
