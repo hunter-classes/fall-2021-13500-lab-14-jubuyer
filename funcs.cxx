93cdc2826e490f0963ec25e9e802cac948605bd7
jubuyer <jahmed10@stuy.edu>
2022-01-22 20:08:34 -0500

    coded up to pop_back(n)

--- funcs.cxx
+++ funcs.cxx
@@ -9,6 +9,71 @@ MyVector<T>::MyVector() {
 }
 
 template <class T>
+T &MyVector<T>::operator[] (int n) {
+  try {
+    if((n > storage) || (n < 0)) { //storage is the actual number of values in MyVector
+      throw 1;
+    }
+  } catch (int e) {
+    std::cerr << "\x1B[31mErr: Out of Bounds\033[0m" << '\n';
+    exit(0);
+  }
+  return data[n];
+}
+
+template <class T>
+void MyVector<T>::push_back(T item) {
+  if (storage == cap) {
+    resize(cap*2);
+  }
+  data[storage] = item;
+  storage++;
+}
+
+template <class T>
+T MyVector<T>::pop_back() {
+  try {
+    if(storage <= 0) { //storage is the actual number of values in MyVector
+      throw 1;
+    }
+  } catch (int e) {
+    std::cerr << "\x1B[31mErr: Out of Bounds\033[0m" << '\n';
+    exit(0);
+  }
+  storage--;
+  T temp = data[storage];
+  data[storage] = 0;
+
+  return temp;
+}
+
+template <class T>
+T MyVector<T>::pop_back(int n) {
+  try {
+    if((n > storage) || (n < 0)) { //storage is the actual number of values in MyVector
+      throw 1;
+    }
+  } catch (int e) {
+    std::cerr << "\x1B[31mErr: Out of Bounds\033[0m" << '\n';
+    exit(0);
+  }
+
+  T *arr = new T[cap];
+  T temp = data[n];
+  for(int i = 0; i < storage; i++) {
+    if(data[i] != data[n]) {
+      arr[i] = data[i];
+    }
+  }
+
+  delete[] data;
+  data = arr;
+  
+  return temp;
+}
+
+
+template <class T>
 int MyVector<T>::size() {
   return storage;
 }
@@ -22,6 +87,7 @@ template <class T>
 void MyVector<T>::resize(int renew) {
   cap = renew;
   T *arr = new T[renew];
+  delete[] data;
   data = arr;
 }
 
